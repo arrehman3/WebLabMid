a77dabdfadbd2164e2bb7552fced786d1d3a63aa
Abdur Rehman <107047867+arrehman3@users.noreply.github.com>
2025-05-12 20:52:00 +0500

    extra files removed

--- web-mid/backend/index.js
+++ web-mid/backend/index.js
@@ -7,11 +7,13 @@ import Visitor from './models/Visitor.js';
 
 dotenv.config();
 const app = express();
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
