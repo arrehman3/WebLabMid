9ab33d6aa5fb0f29f6ada948ec579ff4644cd232
Abdur Rehman <107047867+arrehman3@users.noreply.github.com>
2025-05-12 23:50:47 +0500

    .

--- web-mid/backend/index.js
+++ web-mid/backend/index.js
@@ -8,15 +8,11 @@ import Visitor from './models/Visitor.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://100.27.195.165:3000'
-  ]
-}));
+
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://mongodb:27017/secure-nest')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
@@ -34,4 +30,4 @@ app.get('/api/visitors', async (req, res) => {
   res.json(visitors);
 });
 
-app.listen(PORT,'0.0.0.0',() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
